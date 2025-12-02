d14ab505107604ffe61225a164eeaa229484bff8
Saikumarsiruvati <siruvatisai22@gmail.com>
2025-12-02 20:47:13 +0530

    Initial Commit

--- src/main/java/com/example/JenkinsDemo/JenkinsDemoApplication.java
+++ src/main/java/com/example/JenkinsDemo/JenkinsDemoApplication.java
@@ -2,12 +2,19 @@ package com.example.JenkinsDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
 
+    @GetMapping("/home")
+    public String home() {
+        return "Welcome to Home!";
+    }
 }
